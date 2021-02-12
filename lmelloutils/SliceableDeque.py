699844f05c07083cfbd312d95da81ccc8c40a996
Lucashsmello <lucashsmello@gmail.com>
2021-02-12 15:36:35 -0300

    SliceableDeque

---
+++ lmelloutils/SliceableDeque.py
@@ -0,0 +1,15 @@
+from collections import deque
+
+class SliceableDeque(deque):
+    def __getitem__(self, s):
+        try:
+            start, stop, step = s.start or 0, s.stop or sys.maxsize, s.step or 1
+        except AttributeError:  # not a slice but an int
+            return super().__getitem__(s)
+        else:
+            try:
+                return list(itertools.islice(self, start, stop, step))
+            except ValueError:  # incase of a negative slice object
+                length = len(self)
+                start, stop = length + start if start < 0 else start, length + stop if stop < 0 else stop
+                return list(itertools.islice(self, start, stop, step))
--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup
 
-modules=['cvutils', 'Point', 'jupyter_utils']
+modules=['cvutils', 'Point', 'jupyter_utils', 'SliceableDeque']
 setup(name='lmello-utils',
-    version='1.1',
+    version='1.2',
     description='Lucas Mello utilities',
     url='https://github.com/Lucashsmello/lmello-utils',
     author='Lucas Mello',
